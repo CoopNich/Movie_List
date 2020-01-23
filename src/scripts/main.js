8bf75801b4db820ef7b6c7a8f9429542e886cf54
Cooper Nichols <coopnich92@gmail.com>
2020-01-23 13:26:45 -0600

    javascript fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,15 @@
 const movieFactory = (movie) => {
     return `
-    <section class="movies>
+    <section class="movies">
     <h4>Title: ${movie.title}</h4>
     <h5>Date: ${movie.yearReleased}</h5>
     </section>
     `
 }
-
-
-
-
+const movieContainer = document.querySelector("#movie-container")
+const addMovieToDom = (movieHTML) => {
+    movieContainer.innerHTML += movieHTML
+}
 
 const getMovies = () => {
 
@@ -20,16 +20,12 @@ const getMovies = () => {
         .then(renderedMovies => {
             renderedMovies.forEach(movie => {
                 console.log(movie)
+                const convertToHTML = movieFactory(movie)
+                addMovieToDom(convertToHTML)
             }
             )
-        
-    const movieContainer = document.querySelector("#movie-container")
-    const addMovieToDom = (movieHTML) => {
-        movieContainer.innerHTML += movieHTML
-    }
-    const convertToHTML = movieFactory(movie)
-    addMovieToDom(convertToHTML)
-})
+          
+        })
 };
 
 
